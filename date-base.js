262c9f4e9ed213e5092b2fc9a9dfef5ca0daff18
Svetlana <Svetlanalegkaya15@gmail.com>
2019-12-15 21:28:20 -0800

    first commit

---
+++ date-base.js
@@ -0,0 +1,13 @@
+const dateBase=[
+  {name:'Bla Bla',
+   age:0,
+   rase:'wite'
+    
+  },
+  {name:'Nick',
+   age:5,
+   rase:'latino'
+    
+  }
+];
+module.exports =dateBase;
---
+++ index.js
@@ -0,0 +1,4 @@
+const dateBase = require('./date-base.js');
+function printNames(array) { array.forEach(el => console.log(el.name)) };
+printNames(dateBase);
+ 
